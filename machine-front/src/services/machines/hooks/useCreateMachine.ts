2082a44568926c9f284a2bb6eb33785d6a9f9869
KauanPaulinoAT <kauan.paulino@atgroup.com.br>
2025-07-25 11:13:06 -0300

    :recycle: refactor: (machines) refatoração dos hooks react query useMachine e useCreateMachine

--- machine-front/src/services/machines/hooks/useCreateMachine.ts
+++ machine-front/src/services/machines/hooks/useCreateMachine.ts
@@ -1,10 +1,15 @@
-import { useMutation, useQueryClient } from "react-query";
-import { createNewMachine } from '../api/machineService';
+"use client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createNewMachine } from "../api/machineService";
+import { MachineRuleZod } from "@/schemas/machineSchema";
 
 export function useCreateMachine() {
-    const queryClient = useQueryClient();
-    
-    return useMutation(createNewMachine, {
-        onSuccess: () => {queryClient.invalidateQueries(['machines']);},
-    });
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data: MachineRuleZod) => createNewMachine(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["machines"] });
+    },
+  });
 }
