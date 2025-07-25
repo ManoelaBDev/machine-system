ea1200759dd65d57ea9a79edf69232933271e4bb
KauanPaulinoAT <kauan.paulino@atgroup.com.br>
2025-07-25 11:40:31 -0300

    :bricks: ci (componentização) implementação dos containers com lógica

--- machine-front/src/components/containers/MachineList.tsx
+++ machine-front/src/components/containers/MachineList.tsx
@@ -1,7 +1,27 @@
-export const MachineList = () => {
+"use client";
+
+import { useMachines } from '@/services/machines/hooks/useMachines';
+import MachineTable from '@/components/Machine/MachineTable';
+import { CircularProgress, Alert, Box } from '@mui/material';
+
+export default function MachineList() {
+  const { data, isLoading, isError, error } = useMachines();
+
+  if (isLoading) {
     return (
-        <div>
-            Machine List Component
-        </div>
-    )
+      <Box display="flex" justifyContent="center" mt={4}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (isError) {
+    return (
+      <Alert severity="error">
+        Erro ao carregar máquinas: {(error as Error).message}
+      </Alert>
+    );
+  }
+
+  return <MachineTable machines={data ?? []} />;
 }
