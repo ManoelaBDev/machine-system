9bc61d06f59b67c8e70a7973e07f33a14612a470
KauanPaulinoAT <kauan.paulino@atgroup.com.br>
2025-07-27 20:08:41 -0300

    :lipstick:	feat (MUI na homepage) aplicando MUI na home page do projeto

--- machine-front/src/app/page.tsx
+++ machine-front/src/app/page.tsx
@@ -1,9 +1,25 @@
+"use client";
+
+import { Button, Typography, Stack } from "@mui/joy";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <div>
-      <h1>Bem-vindo ao sistema de Gerenciamento de Máquinas</h1>
-      <p>Atualmente estamos na Home Page.</p>
-      <p>Navegue até a seção de máquinas para gerenciá-las.</p>
-    </div>
+    <Stack spacing={2} alignItems="center" justifyContent="center" minHeight="60vh">
+      <Typography level="h2">Bem-vindo ao sistema de Gerenciamento de Máquinas</Typography>
+      <Typography>Atualmente estamos na Home Page.</Typography>
+      <Typography>Navegue até a seção de máquinas para gerenciá-las.</Typography>
+
+      <Button
+        onClick={() => router.push("/pages/machines")}
+        variant="solid"
+        color="primary"
+        size="lg"
+      >
+        Ir para Máquinas
+      </Button>
+    </Stack>
   );
 }
