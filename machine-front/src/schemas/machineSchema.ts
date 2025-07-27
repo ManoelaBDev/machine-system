6e8a6861c229f0bb0ae753d17bc6c1f6b79f4e3d
KauanPaulinoAT <kauan.paulino@atgroup.com.br>
2025-07-27 20:18:57 -0300

    :recycle: refactor (atributo status) atributo status adicionado no zodschema das máquinas

--- machine-front/src/schemas/machineSchema.ts
+++ machine-front/src/schemas/machineSchema.ts
@@ -3,9 +3,13 @@ import { z } from "zod";
 export const machineSchema = z.object({
     name: z
         .string()
-        .min(3, { message: "Nome precisa de ao menos 3 caracteres" }),
+        .min(1, { message: "Nome da máquina precisa ser fornecido" }),
     tipo: z
-        .string(),
+        .string()
+        .min(1, { message: "Tipo da máquina precisa ser fornecido" }),
+    status: z
+        .string()
+        .min(1, { message: "Status precisa ser fornecido" }),
 });
 
 export type MachineRuleZod = z.infer<typeof machineSchema>;
