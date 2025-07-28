117904bec3c061fec2d378e35df44ae5b3757087
manoela <manoela.santos@atgroup.com.br>
2025-07-28 11:16:53 -0300

    ✨feat: adicionando configuraçao cors

--- machine-backend/src/index.ts
+++ machine-backend/src/index.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import machineRoutes from "./routes/machineRoutes";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://192.168.18.33:3000', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/", machineRoutes);
@@ -14,6 +22,6 @@ app.use("/health", (req, res) => {
   });
 });
 
-app.listen(3333, () => {
-  console.log("🚀 API rodando em http://localhost:3333");
+app.listen(3333, '0.0.0.0', () => {
+  console.log("🚀 API rodando em http://0.0.0.0:3333");
 });
